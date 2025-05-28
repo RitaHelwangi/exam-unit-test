2d2d529f1bde64069a04c877cf9a8a3502bc2c4e
Rita Helwangi <rita.helwangi@gmail.com>
2025-05-28 15:25:16 +0200

    Refactor: addToCart test

--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -6,23 +6,17 @@ describe("Cart", () => {
     // Denna kod körs före varje test. Det är för att rensa kundvagnen, så inte saker ligger kvar från föregående test.
     clearCart();
   });
-
-  // -------------------------------------------------- //
   // Skriv dina testfall här
-
   // Du får ett test att börja med
-  test("addToCart lägger till en ny produkt i kundvagnen", () => {
+  test("addToCart adds a new product to the cart", () => {
     const itemCountBefore = getCartItemCount();
     const input = { id: 1002, name: "Vattenpistol", price: 40 };
-
-    // addToCart returnerar inget - den påverkar kundvagnen
-    // vi behöver använda getCartItemCount för att se om det har lagts till en ny produkt i kundvagnen
     addToCart(input);
     const itemCountAfter = getCartItemCount();
-
     expect(itemCountAfter).toBe(itemCountBefore + 1);
   });
-  test("getCartItemCount returnes 0 when the cart is empty", () => {
+
+  test("getCartItemCount returns 0 when the cart is empty", () => {
     expect(getCartItemCount()).toBe(0);
   });
 });
